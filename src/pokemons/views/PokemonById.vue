6638ede965c1eafbf26f1d5fab2d82b3fa9482bb
manetoso <manetoso98@hotmail.com>
2024-05-31 22:29:39 -0600

    feat(pokemon): add pokemon module

---
+++ src/pokemons/views/PokemonById.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h2>Pokemon by ID</h2>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/shared/views/HomeView.vue'
+import AboutView from '@/shared/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,17 +11,18 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
       path: '/counter',
       name: 'counter',
       component: () => import('../counter/views/CounterView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/:pathMatch(.*)*',
+      redirect: { name: 'home' }
     }
   ]
 })
